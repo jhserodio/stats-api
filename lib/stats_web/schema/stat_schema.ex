e995b8c596e763f8a87e2a33ea4db9dbed91d50f
jhserodio <jhserodio@gmail.com>
2019-12-09 05:08:54 -0300

    feat (stats): add queries stats_by

--- lib/stats_web/schema/stat_schema.ex
+++ lib/stats_web/schema/stat_schema.ex
@@ -9,6 +9,28 @@ defmodule StatsWeb.Schema.Stat do
             arg :final_timestamp, :datetime, default_value: DateTime.from_naive!(~N[9999-01-01T01:01:01Z], "Etc/UTC")
             resolve &StatsResolver.stats_total/3
         end
+
+        field :stats_by_website, :stats do
+            arg  :initial_timestamp, :datetime, default_value: DateTime.from_naive!(~N[0001-01-01T01:01:01Z], "Etc/UTC")
+            arg :final_timestamp, :datetime, default_value: DateTime.from_naive!(~N[9999-01-01T01:01:01Z], "Etc/UTC")
+            arg :min_age, :integer
+            arg :max_age, :integer
+            arg :gender, :gender
+            arg :users, list_of(:email)
+            arg :websites, list_of(:url)
+            resolve &StatsResolver.stats_total/3
+        end
+
+        field :stats_by_user, :stats do
+            arg  :initial_timestamp, :datetime, default_value: DateTime.from_naive!(~N[0001-01-01T01:01:01Z], "Etc/UTC")
+            arg :final_timestamp, :datetime, default_value: DateTime.from_naive!(~N[9999-01-01T01:01:01Z], "Etc/UTC")
+            arg :min_age, :integer
+            arg :max_age, :integer
+            arg :gender, :gender
+            arg :users, list_of(:email)
+            arg :websites, list_of(:url)
+            resolve &StatsResolver.stats_total/3
+        end
     end
   end
   
