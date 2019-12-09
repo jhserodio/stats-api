b8ca6eccd7a58cb3fe6524faaf1fdd0bad93be83
Joao Henrique Serodio Ulbinski <joao.ulbinski@iesde.com.br>
2019-12-09 09:08:37 -0300

    tests (stats): add stats_total tests

--- test/api/stats.test.js
+++ test/api/stats.test.js
@@ -1,5 +1,5 @@
 const GraphQLClient = require('graphql-request').GraphQLClient;
-const sortNumbers = require('sort-numbers');
+const timestamp = require('time-stamp');
 
 describe('Stats Tests', function() {
     const self = this;
@@ -44,9 +44,76 @@ describe('Stats Tests', function() {
                 expect(res.statsTotal.visitsCount).toBeDefined();
                 expect(res.statsTotal.usersCount).toBeDefined();
                 expect(res.statsTotal.websitesCount).toBeDefined();
+                expect(res.statsTotal.visits).toBeDefined();
                 expect(res.statsTotal.users).toBeDefined();
                 expect(res.statsTotal.websites).toBeDefined();
                 done();
             })
     });
+
+    it('StatsTotal - with initial date', done => {
+        const dateVisit = "2019-04-17T14:00:00Z";
+
+        const query = `{
+            statsTotal(
+                initialTimestamp: "${dateVisit}"
+            ) {
+                visits {
+                    timestamp
+                }
+            }
+        }`;
+
+        self.client.request(query)
+            .then(res => {
+                const checkTimeStamp =
+                    res.statsTotal.visits.every(visit => {
+                        const visitTimeStamp = new Date(timestamp.utc(visit.timestamp))
+                        const currentDate = new Date(timestamp.utc(dateVisit))
+
+                        return visitTimeStamp >= currentDate 
+                    });
+
+                expect(res.statsTotal).toBeDefined();
+                expect(checkTimeStamp).toBeTruthy();
+                expect(res.statsTotal.visits).toBeDefined();
+                
+
+                done();
+            })
+    });
+
+    it('StatsTotal - with final date', done => {
+        const dateVisit = "2019-04-17T14:00:00Z";
+
+        const query = `{
+            statsTotal(
+                finalTimestamp: "${dateVisit}"
+            ) {
+                visits {
+                    timestamp
+                }
+            }
+        }`;
+
+        self.client.request(query)
+            .then(res => {
+                const checkTimeStamp =
+                    res.statsTotal.visits.every(visit => {
+                        const visitTimeStamp = new Date(timestamp.utc(visit.timestamp))
+                        const currentDate = new Date(timestamp.utc(dateVisit))
+
+                        return visitTimeStamp <= currentDate 
+                    });
+
+                expect(res.statsTotal).toBeDefined();
+                expect(checkTimeStamp).toBeTruthy();
+                expect(res.statsTotal.visits).toBeDefined();
+                
+
+                done();
+            })
+    });
+
+    
 });
